4d931bb86f922dcf2cf40ede46d064538140ccfe
vidhusan12 <vidhusan709@gmail.com>
2025-06-06 14:08:05 +1200

    Added the routes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
 // Add react-router-dom imports
 
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='' element={<Root />}>
+      <Route path='' element={<HomePage /> } />
+      <Route path='' element={<SearchPage /> } />
+      <Route path='' element={<PetDetailsPage /> } />
+      <Route path='' element={<PetDetailsNotFound /> } />
+    </Route>
+  )
+)
 
 function App() {
   return (
     // replace below with a Router Provider
-    <p>REPLACE ME</p>
+    <RouterProvider router={appRouter}/>
   );
 }
 
